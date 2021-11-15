b5618784ab60daeaaecaa9a8d1eb5ecf62e9dc91
hiagrawal <agrawal.hina13@gmail.com>
2021-11-15 12:37:20 +0530

    Adding Pagination

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -57,7 +57,7 @@ class Feed extends Component {
     const graphqlQuery = {
       query: `
       {
-        posts {
+        posts(page: ${page}) {
           posts {_id title content imageUrl creator {name} createdAt}
           totalPosts
         }
@@ -221,6 +221,7 @@ class Feed extends Component {
             );
             updatedPosts[postIndex] = post;
           } else {
+            updatedPosts.pop();
             updatedPosts.unshift(post);
           }
           return {
